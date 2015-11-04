de564ca0cddbbdc5914810f7aa31b2981a46cd85
Michał Budzyński <michal@virtualdesign.pl>
2015-11-04 01:01:00 +0100

    :whale2: use modules in main file

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 'use strict';
-var svnode = function() {
-  var svn = require('svn-interface');
-
-  svn.status('', {}, function(err, res){
-    console.log(res.status.target.entry.map(function(entry){
-      return entry['wc-status']._attribute.item + ' - ' + entry._attribute.path;
-    }));
-  });
+var svnode = {
+  status: require('./lib/status')
 };
 
-module.export = svnode;
+svnode.status();
