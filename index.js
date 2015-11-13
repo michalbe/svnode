807dd370d13c644b8142fcadb5eaf7c17ceaf5e5
Michał Budzyński <michal@virtualdesign.pl>
2015-11-13 01:01:00 +0100

    :izakaya_lantern: Menu

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 'use strict';
-var svnode = {
-  status: require('./lib/status')
-};
+;(function() {
+  var inquirer = require('inquirer');
+  var svnode = {
+    status: require('./lib/status')
+  };
 
-svnode.status();
+  // Clear screen
+  console.log('\u001b[2J\u001b[0;0H');
+
+  inquirer.prompt([{
+    type: 'list',
+    name: 'action',
+    message: 'SVN actions',
+    choices: [
+      'Status',
+      'Blame',
+      'Log',
+      'Commit',
+      new inquirer.Separator(),
+      'Quit'
+    ]
+  }], function( answers ) {
+    switch (answers.action) {
+      case 'Status':
+        svnode.status();
+        break;
+      case 'Quit':
+        break;
+    }
+  });
+})();
